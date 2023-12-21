d1c51b3f141798b30a541dffdc563e892f342cfd
unknown <you@example.com>
2023-12-21 11:27:49 -0300

    configurate java app

--- src/main/java/com/solvd/library/Main.java
+++ src/main/java/com/solvd/library/Main.java
@@ -1,12 +1,16 @@
-package com.solvd;
+package com.solvd.library;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        LOGGER.info("Hello and welcome!");
 
         // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
